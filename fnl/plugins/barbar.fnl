a1479df93c45c420c31d37670acf0558a5953be0
Felipe Velasquez <felipevelasquez@canals.ai>
2024-12-01 22:33:49 -0500

    set barbar keymaps

--- fnl/plugins/barbar.fnl
+++ fnl/plugins/barbar.fnl
@@ -3,4 +3,9 @@
 
 [{1 :romgrk/barbar.nvim
   :dependencies [:lewis6991/gitsigns.nvim :nvim-tree/nvim-web-devicons]
-  :version :^1.0.0}]
+  :version :^1.0.0
+  :config (fn []
+            (let [default-opts {:noremap true :silent true}] 
+              (vim.api.nvim_set_keymap "n" "<A-,>" "<Cmd>BufferPrevious<CR>" default-opts)
+              (vim.api.nvim_set_keymap "n" "<A-.>" "<Cmd>BufferNext<CR>" default-opts)
+              (vim.api.nvim_set_keymap "n" "<A-c>" "<Cmd>BufferClose<CR>" default-opts)))}]
--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -2,4 +2,10 @@
 local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local nvim = autoload("nvim")
-return {{"romgrk/barbar.nvim", dependencies = {"lewis6991/gitsigns.nvim", "nvim-tree/nvim-web-devicons"}, version = "^1.0.0"}}
+local function _2_()
+  local default_opts = {noremap = true, silent = true}
+  vim.api.nvim_set_keymap("n", "<A-,>", "<Cmd>BufferPrevious<CR>", default_opts)
+  vim.api.nvim_set_keymap("n", "<A-.>", "<Cmd>BufferNext<CR>", default_opts)
+  return vim.api.nvim_set_keymap("n", "<A-c>", "<Cmd>BufferClose<CR>", default_opts)
+end
+return {{"romgrk/barbar.nvim", dependencies = {"lewis6991/gitsigns.nvim", "nvim-tree/nvim-web-devicons"}, version = "^1.0.0", config = _2_}}
